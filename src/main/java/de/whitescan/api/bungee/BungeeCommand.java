dd2a5034ab04d3f92acc705c1ec13b3844c31ead
Whitescan <whitescan.minecraft@gmail.com>
2022-05-19 12:35:15 +0200

    feat: refactoring

--- src/main/java/de/whitescan/api/bungee/BungeeCommand.java
+++ src/main/java/de/whitescan/api/bungee/BungeeCommand.java
@@ -20,14 +20,14 @@ public abstract class BungeeCommand extends Command implements TabExecutor {
 	@Getter
 	private TextComponent noPermissionMessage;
 
-	public BungeeCommand(String name, String permission, String[] aliases, TextComponent noPermissionMessage) {
-		super(name, permission, aliases);
+	public BungeeCommand(String name, String permission, TextComponent noPermissionMessage) {
+		super(name, permission, new String[] {});
 		this.noPermissionMessage = noPermissionMessage;
 		setPermissionMessage(noPermissionMessage.getText());
 	}
 
-	public BungeeCommand(String name, String permission, TextComponent noPermissionMessage) {
-		super(name, permission, new String[] {});
+	public BungeeCommand(String name, String permission, TextComponent noPermissionMessage, String[] aliases) {
+		super(name, permission, aliases);
 		this.noPermissionMessage = noPermissionMessage;
 		setPermissionMessage(noPermissionMessage.getText());
 	}
